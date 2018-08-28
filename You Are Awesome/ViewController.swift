23960eda2b6f871245caabafe80ace966c832a6b
onlineBCHeights <online@bcheights.com>
2018-08-28 16:20:34 -0400

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Griffin Elliott on 8/28/18.
+//  Copyright © 2018 Griffin Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
